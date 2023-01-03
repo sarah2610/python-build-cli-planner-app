78f239d313a414124ece8f87af50b8b539aeb856
sarah2610 <67005823+sarah2610@users.noreply.github.com>
2023-01-03 15:25:32 +0530

    Update reminder.py

--- src/reminder.py
+++ src/reminder.py
@@ -1,7 +1,6 @@
-class PrefixedReminder:
-    """This class acts as a base class for other types of reminders.
-    Classes that subclass it should override the `self.text` property
-    """
-    def __init__(self, prefix="Hey, don't forget to "):
-        self.prefix = prefix
-        self.text = prefix + '<placeholder_text>'
+class RegularReminder():
+    def __iter__(self):
+        raise NotImplementedError("Not implemented")
+
+    def __str__(self):
+        raise NotImplementedError("Method not implemented")
