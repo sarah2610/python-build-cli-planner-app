99488a74122409020499e6dd97c740a4e9150251
sarah2610 <67005823+sarah2610@users.noreply.github.com>
2023-01-03 16:12:18 +0530

    Update external_reminders.py

--- src/external_reminders.py
+++ src/external_reminders.py
@@ -11,21 +11,16 @@ class DateTimeReminder(Iterable):
         self.text = text
         self.date = parse(f'{date} {time}')
         self.time = self.date.time()
-
     def __iter__(self):
         return iter([self.text,
                      self.date.strftime("%m/%d/%YT%H:%M:%SZ"),
                      self.time.strftime('%I:%M %p')])
-
     def is_due(self):
         return self.date < datetime.now()
-
-
 class MorningReminder(DateTimeReminder):
     """A reminder that is due at 9am"""
     def __init__(self, text: str, date: str):
         super().__init__(text, date, '9am')
-
 class EveningReminder(DateTimeReminder):
     """A reminder that is due at 8pm"""
     def __init__(self, text: str, date: str):
